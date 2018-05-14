de39781f400d410bf241c33d4550ce937d33cd73
Darian Plumb <dplumb@bitwise.io>
2018-05-14 15:34:58 -0500

    Create REST API process
    Creates the initial Simple Supply REST API. Also includes the API spec.
    All routes listed in the spec currently return a 501 response as there
    is no processing logic.
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

---
+++ rest_api/simple_supply_rest_api/main.py
@@ -0,0 +1,147 @@
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+import argparse
+import asyncio
+import logging
+import sys
+
+from zmq.asyncio import ZMQEventLoop
+
+from sawtooth_sdk.processor.log import init_console_logging
+
+from sawtooth_rest_api.messaging import Connection
+
+from aiohttp import web
+
+from simple_supply_rest_api.route_handler import RouteHandler
+from simple_supply_rest_api.database import Database
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='Starts the Simple Supply REST API')
+
+    parser.add_argument(
+        '-B', '--bind',
+        help='identify host and port for api to run on',
+        default='localhost:8000')
+    parser.add_argument(
+        '-C', '--connect',
+        help='specify URL to connect to a running validator',
+        default='tcp://localhost:4004')
+    parser.add_argument(
+        '-t', '--timeout',
+        help='set time (in seconds) to wait for a validator response',
+        default=500)
+    parser.add_argument(
+        '--db-name',
+        help='The name of the database',
+        default='simple-supply')
+    parser.add_argument(
+        '--db-host',
+        help='The host of the database',
+        default='localhost')
+    parser.add_argument(
+        '--db-port',
+        help='The port of the database',
+        default='5432')
+    parser.add_argument(
+        '--db-user',
+        help='The authorized user of the database',
+        default='sawtooth')
+    parser.add_argument(
+        '--db-password',
+        help="The authorized user's password for database access",
+        default='sawtooth')
+    parser.add_argument(
+        '-v', '--verbose',
+        action='count',
+        default=0,
+        help='enable more verbose output to stderr')
+
+    return parser.parse_args(args)
+
+
+def start_rest_api(host, port, validator_connection, database):
+    loop = asyncio.get_event_loop()
+    validator_connection.open()
+    app = web.Application(loop=loop)
+    app.on_cleanup.append(lambda app: validator_connection.close())
+
+    handler = RouteHandler(loop, validator_connection, database)
+
+    app.router.add_post('/authorization', handler.authorize)
+
+    app.router.add_post('/agents', handler.create_agent)
+    app.router.add_get('/agents', handler.list_agents)
+    app.router.add_get('/agents/{agent_id}', handler.fetch_agent)
+
+    app.router.add_post('/records', handler.create_record)
+    app.router.add_get('/records', handler.list_records)
+    app.router.add_get('/records/{record_id}', handler.fetch_record)
+    app.router.add_post(
+        '/records/{record_id}/transfer', handler.transfer_record)
+    app.router.add_post('/records/{record_id}/update', handler.update_record)
+
+    LOGGER.info('Starting Simple Supply REST API on %s:%s', host, port)
+    web.run_app(
+        app,
+        host=host,
+        port=port,
+        access_log=LOGGER,
+        access_log_format='%r: %s status, %b size, in %Tf s')
+
+
+def main():
+    loop = ZMQEventLoop()
+    asyncio.set_event_loop(loop)
+
+    validator_connection = None
+    try:
+        opts = parse_args(sys.argv[1:])
+
+        init_console_logging(verbose_level=opts.verbose)
+
+        url = opts.connect
+        if "tcp://" not in url:
+            url = "tcp://" + url
+        validator_connection = Connection(url)
+
+        database = Database(
+            opts.db_host,
+            opts.db_port,
+            opts.db_name,
+            opts.db_user,
+            opts.db_password)
+
+        try:
+            host, port = opts.bind.split(":")
+            port = int(port)
+        except ValueError:
+            print("Unable to parse binding {}: Must be in the format"
+                  " host:port".format(opts.bind))
+            sys.exit(1)
+
+        start_rest_api(host, port, validator_connection, database)
+    except Exception as err:  # pylint: disable=broad-except
+        LOGGER.exception(err)
+        sys.exit(1)
+    finally:
+        if validator_connection is not None:
+            validator_connection.close()
