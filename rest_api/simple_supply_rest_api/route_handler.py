0291b095d8b8fe79bc9ed18b1d1eb880e60f9b71
Darian Plumb <dplumb@bitwise.io>
2018-05-14 15:34:58 -0500

    Create REST API process
    Creates the initial Simple Supply REST API. Also includes the API spec.
    All routes listed in the spec currently return a 501 response as there
    is no processing logic.
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

---
+++ rest_api/simple_supply_rest_api/route_handler.py
@@ -0,0 +1,50 @@
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+from aiohttp.web import HTTPNotImplemented
+
+
+class RouteHandler(object):
+    def __init__(self, loop, validator_connection, database):
+        self._loop = loop
+        self._validator_connection = validator_connection
+        self._database = database
+
+    async def authorize(self, request):
+        raise HTTPNotImplemented()
+
+    async def create_agent(self, request):
+        raise HTTPNotImplemented()
+
+    async def list_agents(self, request):
+        raise HTTPNotImplemented()
+
+    async def fetch_agent(self, request):
+        raise HTTPNotImplemented()
+
+    async def create_record(self, request):
+        raise HTTPNotImplemented()
+
+    async def list_records(self, request):
+        raise HTTPNotImplemented()
+
+    async def fetch_record(self, request):
+        raise HTTPNotImplemented()
+
+    async def transfer_record(self, request):
+        raise HTTPNotImplemented()
+
+    async def update_record(self, request):
+        raise HTTPNotImplemented()
