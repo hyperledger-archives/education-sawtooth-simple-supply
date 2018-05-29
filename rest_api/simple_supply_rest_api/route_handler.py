658244db2ce39ed00ab13b14fa0dae56e3d8b94b
Darian Plumb <dplumb@bitwise.io>
2018-05-29 15:15:02 -0500

    Add POST agents route
    - Add example serialization/encryption keys to app
    - Implement logic for validating input for the route
    - Create entry for Auth database
    - Send transaction to messenger
    - Add fetch route for database
    - Update Dockerfile
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- rest_api/simple_supply_rest_api/route_handler.py
+++ rest_api/simple_supply_rest_api/route_handler.py
@@ -12,8 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
+import datetime
+from json.decoder import JSONDecodeError
+import logging
+import time
 
 from aiohttp.web import HTTPNotImplemented
+from aiohttp.web import json_response
+import bcrypt
+from Crypto.Cipher import AES
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+from simple_supply_rest_api.errors import ApiBadRequest
+from simple_supply_rest_api.errors import ApiInternalError
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class RouteHandler(object):
@@ -26,7 +40,41 @@ class RouteHandler(object):
         raise HTTPNotImplemented()
 
     async def create_agent(self, request):
-        raise HTTPNotImplemented()
+        body = await decode_request(request)
+        required_fields = ['name', 'password']
+        validate_fields(required_fields, body)
+
+        public_key, private_key = self._messenger.get_new_key_pair()
+
+        await self._messenger.send_create_agent_transaction(
+            private_key=private_key,
+            name=body.get('name'),
+            timestamp=get_time())
+
+        encrypted_private_key = encrypt_private_key(
+            request.app['aes_key'], public_key, private_key)
+        hashed_password = hash_password(body.get('password'))
+
+        await self._database.create_auth_entry(
+            public_key, encrypted_private_key, hashed_password)
+
+        resource = await self._database.fetch_agent_resource(public_key)
+        if resource is None:
+            raise ApiInternalError(
+                'Transaction committed but not yet reported')
+        agent = {
+            'publicKey': resource['public_key'],
+            'name': resource['name'],
+            'timestamp': resource['timestamp']
+        }
+        authorization = generate_auth_token(
+            request.app['secret_key'], public_key)
+
+        response = {
+            'authorization': authorization,
+            'agent': agent
+        }
+        return json_response(response)
 
     async def list_agents(self, request):
         raise HTTPNotImplemented()
@@ -48,3 +96,38 @@ class RouteHandler(object):
 
     async def update_record(self, request):
         raise HTTPNotImplemented()
+
+
+async def decode_request(request):
+    try:
+        return await request.json()
+    except JSONDecodeError:
+        raise ApiBadRequest('Improper JSON format')
+
+
+def validate_fields(required_fields, body):
+    for field in required_fields:
+        if body.get(field) is None:
+            raise ApiBadRequest(
+                "'{}' parameter is required".format(field))
+
+
+def encrypt_private_key(aes_key, public_key, private_key):
+    init_vector = bytes.fromhex(public_key[:32])
+    cipher = AES.new(bytes.fromhex(aes_key), AES.MODE_CBC, init_vector)
+    return cipher.encrypt(private_key)
+
+
+def hash_password(password):
+    return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def get_time():
+    dts = datetime.datetime.utcnow()
+    return round(time.mktime(dts.timetuple()) + dts.microsecond/1e6)
+
+
+def generate_auth_token(secret_key, public_key):
+    serializer = Serializer(secret_key)
+    token = serializer.dumps({'public_key': public_key})
+    return token.decode('ascii')
