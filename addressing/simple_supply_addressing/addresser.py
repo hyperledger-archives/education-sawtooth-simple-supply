e85b89f670f9f329d908f717ab61650e7b9c6c6d
Darian Plumb <dplumb@bitwise.io>
2018-05-29 15:29:52 -0500

    Implement subscriber and agent event handling
    - Tables changed to bigint to handle max block num
    - Added event handler and subscriber (based off of Marketplace)
    - Added fork resolution and db queries (based off of 0.8 Supply Chain)
    - Added retry connection for database
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- addressing/simple_supply_addressing/addresser.py
+++ addressing/simple_supply_addressing/addresser.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # -----------------------------------------------------------------------------
 
+import enum
 import hashlib
 
 
@@ -23,6 +24,28 @@ AGENT_PREFIX = '00'
 RECORD_PREFIX = '01'
 
 
+@enum.unique
+class AddressSpace(enum.IntEnum):
+    AGENT = 0
+    RECORD = 1
+
+    OTHER_FAMILY = 100
+
+
 def get_agent_address(public_key):
     return NAMESPACE + AGENT_PREFIX + hashlib.sha512(
         public_key.encode('utf-8')).hexdigest()[:62]
+
+
+def get_address_type(address):
+    if address[:len(NAMESPACE)] != NAMESPACE:
+        return AddressSpace.OTHER_FAMILY
+
+    infix = address[6:8]
+
+    if infix == '00':
+        return AddressSpace.AGENT
+    if infix == '01':
+        return AddressSpace.RECORD
+
+    return AddressSpace.OTHER_FAMILY
---
+++ subscriber/simple_supply_subscriber/decoding.py
@@ -0,0 +1,77 @@
+# Copyright 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+from simple_supply_addressing.addresser import AddressSpace
+from simple_supply_addressing.addresser import get_address_type
+from simple_supply_protobuf.agent_pb2 import AgentContainer
+from simple_supply_protobuf.record_pb2 import RecordContainer
+
+
+CONTAINERS = {
+    AddressSpace.AGENT: AgentContainer,
+    AddressSpace.RECORD: RecordContainer
+}
+
+
+def deserialize_data(address, data):
+    """Deserializes state data by type based on the address structure and
+    returns it as a dictionary with the associated data type
+
+    Args:
+        address (str): The state address of the container
+        data (str): String containing the serialized state data
+    """
+    data_type = get_address_type(address)
+
+    if data_type == AddressSpace.OTHER_FAMILY:
+        return []
+
+    try:
+        container = CONTAINERS[data_type]
+    except KeyError:
+        raise TypeError('Unknown data type: {}'.format(data_type))
+
+    entries = _parse_proto(container, data).entries
+    return data_type, [_convert_proto_to_dict(pb) for pb in entries]
+
+
+def _parse_proto(proto_class, data):
+    deserialized = proto_class()
+    deserialized.ParseFromString(data)
+    return deserialized
+
+
+def _convert_proto_to_dict(proto):
+    result = {}
+
+    for field in proto.DESCRIPTOR.fields:
+        key = field.name
+        value = getattr(proto, key)
+
+        if field.type == field.TYPE_MESSAGE:
+            if field.label == field.LABEL_REPEATED:
+                result[key] = [_convert_proto_to_dict(p) for p in value]
+            else:
+                result[key] = _convert_proto_to_dict(value)
+
+        elif field.type == field.TYPE_ENUM:
+            number = int(value)
+            name = field.enum_type.values_by_number.get(number).name
+            result[key] = name
+
+        else:
+            result[key] = value
+
+    return result
