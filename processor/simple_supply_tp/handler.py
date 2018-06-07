e2c31b3ae3c8318defe624f2d4a26d789cf5f769
Darian Plumb <dplumb@bitwise.io>
2018-06-07 14:43:59 -0500

    Implement TP functionality for transfer record
    - TP Transfer Record functionality implemented
    - Reporting database record schema changed to include a unique ID
    - Added unit tests
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- processor/simple_supply_tp/handler.py
+++ processor/simple_supply_tp/handler.py
@@ -65,6 +65,11 @@ class SimpleSupplyHandler(TransactionHandler):
                 state=state,
                 public_key=header.signer_public_key,
                 payload=payload)
+        elif payload.action == payload_pb2.SimpleSupplyPayload.TRANSFER_RECORD:
+            _transfer_record(
+                state=state,
+                public_key=header.signer_public_key,
+                payload=payload)
         else:
             raise InvalidTransaction('Unhandled action')
 
@@ -99,6 +104,35 @@ def _create_record(state, public_key, payload):
         longitude=payload.data.longitude,
         record_id=payload.data.record_id,
         timestamp=payload.timestamp)
+
+
+def _transfer_record(state, public_key, payload):
+    if state.get_agent(payload.data.receiving_agent) is None:
+        raise InvalidTransaction('Agent with the public key {} does '
+                                 'not exist'.format(public_key))
+
+    record = state.get_record(payload.data.record_id)
+    if record is None:
+        raise InvalidTransaction('Record with the record id {} does not '
+                                 'exist'.format(payload.data.record_id))
+
+    if not _validate_record_owner(signer_public_key=public_key,
+                                  record=record):
+        raise InvalidTransaction(
+            'Transaction signer is not the owner of the record')
+
+    state.transfer_record(
+        receiving_agent=payload.data.receiving_agent,
+        record_id=payload.data.record_id,
+        timestamp=payload.timestamp)
+
+
+def _validate_record_owner(signer_public_key, record):
+    """Validates that the public key of the signer is the latest (i.e.
+    current) owner of the record
+    """
+    latest_owner = max(record.owners, key=lambda obj: obj.timestamp).agent_id
+    return latest_owner == signer_public_key
 
 
 def _validate_latlng(latitude, longitude):
