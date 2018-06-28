322269d5c02a55833d664e531ab1835d46f6e0d9
Darian Plumb <dplumb@bitwise.io>
2018-06-28 14:35:54 -0500

    Add and ignore request
    Request argument was removed from route methods which caused an error for
    some GET requests
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

--- rest_api/simple_supply_rest_api/route_handler.py
+++ rest_api/simple_supply_rest_api/route_handler.py
@@ -82,7 +82,7 @@ class RouteHandler(object):
 
         return json_response({'authorization': token})
 
-    async def list_agents(self):
+    async def list_agents(self, _request):
         agent_list = await self._database.fetch_all_agent_resources()
         return json_response(agent_list)
 
@@ -111,7 +111,7 @@ class RouteHandler(object):
         return json_response(
             {'data': 'Create record transaction submitted'})
 
-    async def list_records(self):
+    async def list_records(self, _request):
         record_list = await self._database.fetch_all_record_resources()
         return json_response(record_list)
 
